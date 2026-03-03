bd7b77870b23662a014006f77e0da1296576426d
JohanH <johan.hietala@iths.se>
2026-03-03 12:35:40 +0100

    Added Book entity with required fields and validation

---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,53 @@
+package entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+
+    @NotBlank(message = "Author cannot be empty")
+    private String author;
+
+    @NotBlank
+    @Size(min = 10, max = 13, message = "ISBN must be 10-13 characters")
+    private String isbn;
+
+    @NotBlank(message = "Genre cannot be empty")
+    private String genre;
+
+    @NotBlank(message = "Description cannot be empty")
+    @Size(min = 1, message = "Description must be at least 1 character")
+    private String description;
+
+    @NotBlank(message = "Pagecount cannot be empty")
+    @Size(min = 1, message = "Pagecount must be at least 1 character")
+    private int pageCount;
+
+    @NotBlank(message = "Published date cannot be empty")
+    private LocalDate publishedDate;
+
+    public Book() {
+
+    }
+
+
+
+
+
+}
